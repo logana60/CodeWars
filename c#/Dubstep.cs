360e63a97835fefa097f0cbb195738f2813256c3
Logan Arnold <logana60@gmail.com>
2018-06-13 12:39:57 -0400

    Initial Commit

---
+++ c#/Dubstep.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+	public static class DubstepTests
+	{
+		public class Dubstep
+		{
+			public static string SongDecoder(string input)
+			{
+				input = input.Replace("WUB", " ");
+				input = input.TrimStart(' ');
+				input = input.TrimEnd(' ');
+
+
+
+				return input;
+			}
+		}
+	}
+}
---
+++ c#/PrinterErrors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+	public static class PrinterTests
+	{
+		public class Printer
+		{
+			public static string PrinterError(String s)
+			{
+				float LengthOfString = s.Length;
+				string Acceptable = "abcdefghijklm";
+				int errors = 0;
+
+				s = s.ToLower();
+
+				foreach (char letter in s)
+				{
+					if(!Acceptable.Contains(letter))
+					{
+						errors += 1;
+					}
+				}
+
+				return (errors.ToString() + "/" + LengthOfString.ToString());
+			}
+		}
+	}
+}
